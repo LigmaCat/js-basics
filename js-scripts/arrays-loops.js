ca2eb31a72776f5473949d6318be2a7cb0554bf2
kristaps <you@example.com>
2024-11-05 08:39:35 +0200

    1 exersise done

---
+++ js-scripts/arrays-loops.js
@@ -0,0 +1,18 @@
+// for (let i =11; i <= 20; i++){
+//     console.log(i)
+// }
+
+// for (let i = 20; i >= 11; i--){
+//     console.log(i)
+// }
+
+let draugi = ["Jelgava", "Kūldīga", "Ventspils", "Rīga", "Liepāja"]
+
+// console.log(draugi[0])
+// console.log(draugi[2])
+
+let draugiskaits = draugi.length
+
+for (let i = 0; i <= draugiskaits; i++){
+console.log(draugi[i])
+}
---
+++ js-scripts/variables.js
@@ -0,0 +1,4 @@
+let name = Edgars
+let age = 16
+isStudent = true
+const Pi = 3.14
